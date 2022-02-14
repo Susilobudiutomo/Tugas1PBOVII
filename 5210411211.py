7d41e0ad940e859f99d08af53a75ced190fb236e
Susilobudiutomo <99601365+Susilobudiutomo@users.noreply.github.com>
2022-02-14 23:39:24 +0700

    Tugas1
    Tugas1

---
+++ 5210411211.py
@@ -0,0 +1,12 @@
+#Bangun Datar Segitiga
+alas_segitiga = 4
+tinggi_segitiga = 7
+luas_segitiga = 1 / 2 * (alas_segitiga * tinggi_segitiga)
+print("Luas segitiga = ", luas_segitiga)
+
+#Bangun Ruang balok
+panjang_balok = 10
+lebar_balok = 7
+tinggi_balok = 11
+volume_balok = panjang_balok * lebar_balok * tinggi_balok
+print("volume balok = ",volume_balok)
