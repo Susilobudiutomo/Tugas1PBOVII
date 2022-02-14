31ea15f629ff0a22b67fe66cad15f1658f6bab89
Susilobudiutomo <99601365+Susilobudiutomo@users.noreply.github.com>
2022-02-14 23:17:15 +0700

    Tugas1
    Tugas1

---
+++ 5210411157.py
@@ -0,0 +1,9 @@
+#MENCARI KELILING ALAS TABUNG
+print("========== IDENTITAS ==========")
+Nama = str(input("Input nama anda: "))
+NPM  = int(input("Input NPM anda : "))
+print("===============================")
+
+jari_jari = float(input("Input jari-jari Tabung ---> "))
+hasil = 2 * 3.14 * jari_jari
+print("Keliling alas tabung tersebut adalah ---> ", hasil)
