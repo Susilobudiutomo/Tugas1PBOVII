7a1da4abfb3606b1e27b1165b6f54711dbb41447
Susilobudiutomo <99601365+Susilobudiutomo@users.noreply.github.com>
2022-02-14 23:43:04 +0700

    Tugas1
    Tugas1

---
+++ 5210411226.py
@@ -0,0 +1,15 @@
+#MENGHITUNG LUAS DAN KELILING SEBUAH TRAPESIUM
+
+a = int(input("masukan alasnya                       = "))
+c = int(input("masukan sisi yang sejajar dengan alas = "))
+t = int(input("masukan tingginya                     = "))
+sa = int(input("masukan sisi1                        = "))
+sb = int(input("masukan sisi2                        = "))
+sc = int(input("masukan sisi3                        = "))
+sd = int(input("masukan sisi4                        = "))
+
+luas = 1/2* (a+c) *t
+kel = sa+sb+sc+sd
+
+print("luas trapesium adalah      = " ,luas)
+print("keliling trapesium adalah  = " ,kel )
