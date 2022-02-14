50401ebfb32cdcb02980022e1ccb083b0b9b9551
Susilobudiutomo <99601365+Susilobudiutomo@users.noreply.github.com>
2022-02-14 23:03:03 +0700

    Tugas1
    Tugas1

---
+++ 5210411212.py
@@ -0,0 +1,12 @@
+phi = 3.14
+r = 14
+s = 9
+t = 8
+
+#luas Kerucut
+L = (phi * (r * r)) + (phi * r * s)
+print("Jadi luas permukaan Kerucut = ",L)
+
+#Volume Kerucut
+V = 1/3 * phi * (r * r) * t
+print("Jadi volume permukaan Kerucut = ",V)
