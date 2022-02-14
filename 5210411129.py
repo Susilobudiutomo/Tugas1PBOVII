c8c24f2079d324e8642cff565be0f33cb7782033
Susilobudiutomo <99601365+Susilobudiutomo@users.noreply.github.com>
2022-02-14 23:45:03 +0700

    Tugas1
    Tugas1

---
+++ 5210411129.py
@@ -0,0 +1,5 @@
+#rumus menghitung luas persegi panjang adalah panjang*lebar
+panjang = int(input("masukan angka panjang : "))
+lebar = int(input("masukan angka lebar : "))
+luas = panjang*lebar
+print ("luas persegi panjang tersebut adalah : ",luas)
