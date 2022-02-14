ac541bfb2b52df3549b5ca31a6803eae923f73c9
Susilobudiutomo <99601365+Susilobudiutomo@users.noreply.github.com>
2022-02-14 23:41:55 +0700

    Tugas1
    Tugas1

---
+++ 5210411219.py
@@ -0,0 +1,13 @@
+# diketahui sebuah panjang balok 8 cm, lebar 6 cm, tinggi  4 cm
+# hitung luas permukaan balok
+
+p = 8
+l = 6
+t = 4
+L = 2 * (p*l + p*t + l*t)
+print('diketahui sebuah balok memiliki panjang 8 cm, lebar 6 cm, tinggi  4 cm')
+print('hitung luas permukaan balok')
+print('panjang sebuah balok =',p)
+print('lebar sebuah balok   =',l)
+print('tinggi sebuah balok  =',t)
+print("Luas permukaan balok adalah",L)
